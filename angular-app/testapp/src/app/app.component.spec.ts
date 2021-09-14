d3fd8101133f48fbb324d1be5e346af52e536c45
admin <admin@CHETUIWK0393.chetu.com>
2021-09-14 19:15:16 +0530

    14sep

--- angular-app/testapp/src/app/app.component.spec.ts
+++ angular-app/testapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firstapp'`, () => {
+  it(`should have as title 'testapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstapp');
+    expect(app.title).toEqual('testapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('firstapp app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('testapp app is running!');
   });
 });
